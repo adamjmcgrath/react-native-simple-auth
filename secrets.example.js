d1fca9aed879bd03b79cc13fb418c6af42c05c62
Adam Mcgrath <adamjmcgrath@gmail.com>
2015-05-31 18:18:03 +0100

    add secrets.js example

---
+++ secrets.example.js
@@ -0,0 +1,28 @@
+// Example of provider configs supported by SimpleAuth.
+
+module.exports = {
+  'google-web': {
+    client_id: '',
+    client_secret: ''
+  },
+  facebook: {
+    app_id: ''
+  },
+  twitter: {
+    consumer_key: '',
+    consumer_secret: ''
+  },
+  instagram: {
+    client_id: '',
+    redirect_uri: ''
+  },
+  tumblr: {
+    consumer_key: '',
+    consumer_secret: ''
+  },
+  'linkedin-web': {
+    client_id: '',
+    client_secret: '',
+    redirect_uri: ''
+  }
+};
